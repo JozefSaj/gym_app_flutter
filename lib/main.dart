f3b859d655f0383d7607fdda58853aaef4588054
Jozef Šajtlava <jozef.sajtlava02@gmail.com>
2023-04-24 00:19:23 +0200

    authentification with firebase, waiting for firebase snapshot to choose action

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:gym_app_flutter/pages/auth_page.dart';
 import 'package:provider/provider.dart';
 import 'data/workout_data.dart';
 import 'pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -17,7 +25,14 @@ class MyApp extends StatelessWidget {
       create: (context) => WorkoutData(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: HomePage(),
+        home: StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(),
+            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator(),);
+          }
+
+          return snapshot.hasData ? const HomePage() : const AuthPage();
+            },),
         theme: ThemeData(useMaterial3: true),
       ),
     );
