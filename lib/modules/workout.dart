1b447b4fb90d0652e2f3eb0d5116de10da72b85c
Jozef Šajtlava <jozef.sajtlava02@gmail.com>
2023-04-25 00:32:42 +0200

    isPublic to workout added

--- lib/modules/workout.dart
+++ lib/modules/workout.dart
@@ -3,6 +3,7 @@ import 'exercise.dart';
 class Workout{
   late final String name;
   late final List<Exercise> exercise;
+  bool isPublic;
 
-  Workout({required this.name, required this.exercise});
+  Workout({required this.name, required this.exercise, this.isPublic = false});
 }
