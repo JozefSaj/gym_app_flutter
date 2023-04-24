9c94b2fb9be593af84e2a6ebe93182509957c46f
Jozef Šajtlava <jozef.sajtlava02@gmail.com>
2023-04-24 22:19:38 +0200

    initialization of attributes

--- lib/modules/user.dart
+++ lib/modules/user.dart
@@ -1,15 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class User with ChangeNotifier{
-  final String name;
-  final String email;
+class User with ChangeNotifier {
+  String name;
+  String email;
+  String id;
   bool isAdmin;
 
-  User({required this.name, required this.email, this.isAdmin = false});
+  User({this.name = '', this.isAdmin = false, this.email = '', this.id = ''});
 
+  Future<void> init({required id}) async {
+    final response =
+        await FirebaseFirestore.instance.collection('users').doc(id).get();
 
-  User.fromSnapshot(String email, String password) {
-
+    isAdmin = response.data()!['isAdmin'];
+    email = response.data()!['email'];
   }
-
 }
