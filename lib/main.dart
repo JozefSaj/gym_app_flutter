89a9a6eb431f4ff143e7e7ee4f14526175266639
Jozef Šajtlava <jozef.sajtlava02@gmail.com>
2023-04-24 22:17:55 +0200

    multi provider for feedbackData

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart' hide User;
 import 'package:flutter/material.dart';
+import 'package:gym_app_flutter/data/feedback_data.dart';
 import 'package:gym_app_flutter/pages/auth_page.dart';
 import 'package:provider/provider.dart';
 import 'data/workout_data.dart';
 import 'pages/home_page.dart';
+import 'modules/user.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
@@ -21,18 +23,45 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => WorkoutData(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => WorkoutData(),
+        ),
+        ChangeNotifierProvider(create: (context) => FeedbackData(),)
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: StreamBuilder(stream: FirebaseAuth.instance.authStateChanges(),
-            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator(),);
-          }
+        home: StreamBuilder(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (!snapshot.hasData) {
+              return AuthPage();
+            }
+            final user = User();
+            return FutureBuilder(
+              future: user.init(id: FirebaseAuth.instance.currentUser!.uid),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return ChangeNotifierProvider.value(
+                  value: user,
+                  child: HomePage(),
+                );
+              },
+            );
 
-          return snapshot.hasData ? const HomePage() : const AuthPage();
-            },),
+            return snapshot.hasData ? const HomePage() : const AuthPage();
+          },
+        ),
         theme: ThemeData(useMaterial3: true),
       ),
     );
