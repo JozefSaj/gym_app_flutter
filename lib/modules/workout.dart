65726027fd1452c6d9bee84139301157cd096f90
Jozef Šajtlava <jozef.sajtlava02@gmail.com>
2023-05-02 19:46:58 +0200

    late no needed

--- lib/modules/workout.dart
+++ lib/modules/workout.dart
@@ -1,8 +1,8 @@
 import 'exercise.dart';
 
 class Workout{
-  late final String name;
-  late final List<Exercise> exercise;
+  String name;
+  final List<Exercise> exercise;
   bool isPublic;
 
   Workout({required this.name, required this.exercise, this.isPublic = false});
